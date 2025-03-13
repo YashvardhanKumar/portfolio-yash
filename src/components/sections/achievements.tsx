d738c3198932b0df7d3b7567fb95b8c17192bbf2
Yashvardhan Kumar <79046841+YashvardhanKumar@users.noreply.github.com>
2025-03-14 01:15:49 +0530

    Update achievements.tsx

--- src/components/sections/achievements.tsx
+++ src/components/sections/achievements.tsx
@@ -2,36 +2,24 @@ const Achievements = () => {
   return (
     <div className="flex flex-col w-full md:p-10 p-2 gap-5 items-center">
       <p className="flex flex-wrap gap-5 justify-center items-center w-full">
+        
         <img
           src="https://leetcard.jacoblin.cool/YashvardhanKumar?theme=dark&font=Poppins&ext=contest&show_rank=false"
           alt=""
           className="max-w-96 w-full"
         />
-
         <img
           src="https://github-readme-stats.vercel.app/api/top-langs?username=YashvardhanKumar&show_icons=true&locale=en&layout=compact&theme=highcontrast&hide_border=true"
           alt="yashvardhankumar"
           className="max-w-96 w-full"
         />
-        &nbsp;
         <img
           src="https://github-readme-stats.vercel.app/api?username=YashvardhanKumar&show_icons=true&locale=en&layout=compact&theme=highcontrast&hide_border=true"
           alt="yashvardhankumar"
           className="max-w-96 w-full"
         />
-        <a href="https://git.io/streak-stats">
-          <img src="https://streak-stats.demolab.com?user=YashvardhanKumar&theme=highcontrast&hide_border=true&border_radius=10&date_format=M%20j%5B%2C%20Y%5D" alt="GitHub Streak" className="max-w-96 w-full" />
-        </a>
-        {" "}
-          {/*
-          <a href="https://git.io/streak-stats">
-          <img
-            src="https://github-readme-streak-stats.herokuapp.com?user=YashvardhanKumar&theme=highcontrast&hide_border=true&border_radius=10&date_format=M%20j%5B%2C%20Y%5D"
-            alt="GitHub Streak"
-          className="max-w-96 w-full"
-          />
-        </a>
-          */}
+{/*         <a href="https://git.io/streak-stats"><img src="https://streak-stats.demolab.com?user=YashvardhanKumar&theme=highcontrast&hide_border=true" className="max-w-96 w-full" alt="GitHub Streak" /></a> */}
+        <img src="https://streak-stats.demolab.com?user=YashvardhanKumar&theme=highcontrast&hide_border=true&border_radius=10&fire=EB5454" className="max-w-96 w-full" alt="GitHub Streak" />
       </p>{" "}
       <a href="https://github.com/ryo-ma/github-profile-trophy">
           <img
