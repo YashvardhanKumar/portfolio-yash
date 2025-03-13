4ce63ab36bc494538a59f8d280c487016f8da74d
Yashvardhan Kumar <79046841+YashvardhanKumar@users.noreply.github.com>
2025-03-14 01:07:37 +0530

    Update achievements.tsx

--- src/components/sections/achievements.tsx
+++ src/components/sections/achievements.tsx
@@ -22,9 +22,9 @@ const Achievements = () => {
         <a href="https://git.io/streak-stats">
           <img src="https://streak-stats.demolab.com?user=YashvardhanKumar&theme=highcontrast&hide_border=true&border_radius=10&date_format=M%20j%5B%2C%20Y%5D" alt="GitHub Streak" className="max-w-96 w-full" />
         </a>
-        <{" "}
+        {" "}
           {/*
-          a href="https://git.io/streak-stats">
+          <a href="https://git.io/streak-stats">
           <img
             src="https://github-readme-streak-stats.herokuapp.com?user=YashvardhanKumar&theme=highcontrast&hide_border=true&border_radius=10&date_format=M%20j%5B%2C%20Y%5D"
             alt="GitHub Streak"
